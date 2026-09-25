2eb5e0c1466369de1044b6ffac85c218d03221bc
michaeljaya <michaeljaya285@gmail.com>
2026-09-25 17:19:27 +0700

    Merapikan tampilan di Tentang Kami

--- src/app/tentang/tentang.page.ts
+++ src/app/tentang/tentang.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tentang',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentangPage implements OnInit {
 
-  constructor() { }
+  constructor( private animationCtrl: AnimationController) { }
 
   ngOnInit() {
   }
 
+  fadeInAvatar() {
+    const avatarElement = document.querySelector('#myAvatar') as HTMLElement;
+    const animation = this.animationCtrl
+      .create()
+      .addElement(avatarElement)
+      .duration(1000) 
+      .keyframes([
+        { offset: 0, opacity: '0' }, 
+        { offset: 0.2, opacity: '0.2' },
+        { offset: 0.4, opacity: '0.4' },
+        { offset: 0.6, opacity: '0.6' },
+        { offset: 0.8, opacity: '0.8' },
+        { offset: 1, opacity: '1' },
+      ]);
+    animation.play();
+  }
+
+  ionViewDidEnter() {
+    this.fadeInAvatar();
+  }
 }
