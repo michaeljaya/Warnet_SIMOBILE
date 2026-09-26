a9f03c4b020b25edbac838cea1493a682dc13956
richardgsr <s160424099@student.ubaya.ac.id>
2026-09-26 22:04:43 +0700

    menambahkan edit produk di produknya dan menambahkan menu baru yaitu tambah produk

--- src/app/produkform/produkform.module.ts
+++ src/app/produkform/produkform.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular/lazy';
 
@@ -12,9 +12,10 @@ import { ProdukformPage } from './produkform.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     ProdukformPageRoutingModule
   ],
   declarations: [ProdukformPage]
 })
-export class ProdukformPageModule {}
+export class ProdukformPageModule { }
--- src/app/produkform/produkform.page.ts
+++ src/app/produkform/produkform.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product';
 
 @Component({
   selector: 'app-produkform',
@@ -7,10 +11,68 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ProdukformPage implements OnInit {
+  productForm: FormGroup;
+  isEdit = false;
+  editId: number = 0;
 
-  constructor() { }
+  alertButtons = [{
+    text: 'OK',
+    handler: () => {
+      this.router.navigate(['/produk']);
+    }
+  }];
+
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      category: ['', Validators.required],
+      buyPrice: [0, [Validators.required, Validators.min(0)]],
+      sellPrice: [0, [Validators.required, Validators.min(0)]],
+      stock: [0, [Validators.required, Validators.min(0)]],
+      description: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+    // cek apakah ada paramter id 
+    this.route.params.subscribe(params => {
+      if (params['id']) {
+        this.isEdit = true;
+        this.editId = Number(params['id']);
+        // Ambil data produk yang mau kita edit lalu isi form nya
+        const product = this.productService.produkId(this.editId);
+        if (product) {
+          this.productForm.patchValue({
+            name: product.name,
+            category: product.category,
+            buyPrice: product.buyPrice,
+            sellPrice: product.sellPrice,
+            stock: product.stock,
+            description: product.description,
+          });
+        }
+      }
+    });
+  }
+
+  save() {
+    if (this.productForm.valid) {
+      const v = this.productForm.value;
+      // Hanya jalankan logika update produk
+      if (this.isEdit) {
+        //update produk 
+        this.productService.updateProduk(this.editId, v.name, v.category, v.buyPrice, v.sellPrice, v.stock, v.description);
+      } else {
+        //tambah produk
+        this.productService.tambahProduk(v.name, v.category, v.buyPrice, v.sellPrice, v.stock, v.description);
+      }
+    }
   }
 
 }
+
